5bfe32dc763b875f42b0772fbec3428cee13faad
Alex Gonzalez <alexgonrod83@gmail.com>
2018-02-12 12:22:51 +0100

    Adding card

---
+++ thepinkpanther/src/components/card/card.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+    render() 
+    return <div className="flip-container">
+        <div className="front">
+            <img src="" alt=""/>
+            <div className="footer">
+                <p><strong>Title</strong><br/>
+                    Film category
+                </p>
+            </div>
+        </div>
+            <div className="back">
+                <p><strong>Film title</strong>
+                    <br/>
+                    <br/> Film info <br/> More info <br/> Much more info
+                </p>
+            </div>
+    </div>
+    
+
+
+export default Card
