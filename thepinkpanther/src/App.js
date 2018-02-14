03a83e4ea2e882552bbc3591d56e2c1205a2421d
Alex Gonzalez <alexgonrod83@gmail.com>
2018-02-14 09:05:18 +0100

    Add README

--- thepinkpanther/src/App.js
+++ thepinkpanther/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
+import Footer from './components/Footer'
 import { HashRouter } from 'react-router-dom'
 
 
