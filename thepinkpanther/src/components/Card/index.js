78d1afac750e16fc0d364c7ac65f697df897108a
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-02-15 09:30:40 +0100

    mergin

--- thepinkpanther/src/components/Card/index.js
+++ thepinkpanther/src/components/Card/index.js
@@ -9,14 +9,8 @@ function Card(props) {
             <img src={(props.item.poster_path) ? `http://image.tmdb.org/t/p/w500/${props.item.poster_path}` : no_image} alt={(props.item.title) ? props.item.title : props.item.name} />
         </div>
         <div className="back">
-<<<<<<< HEAD
             <h3><strong>{(props.item.title) ? props.item.title : props.item.name}</strong></h3>
             <p> {(props.item.overview) ? props.item.overview.substring(0, 300).concat("...") : undefined}</p>
-=======
-            <h3><strong>{(props.itemActiveFilms.title) ? props.itemActiveFilms.title : props.itemActiveFilms.name}</strong></h3>
-                <p> {props.itemActiveFilms.overview.substring(0,200).concat("...")}</p>
-            <button class="btn my-2 my-sm-0">Series</button>
->>>>>>> 0acb675796460d226bfab000a1d83151fd6003eb
         </div>
     </div>)
 }
