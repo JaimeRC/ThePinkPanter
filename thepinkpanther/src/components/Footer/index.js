28f2c59004dc702a3d3b6100ad88d8f74ab738b2
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-02-15 17:29:18 +0100

    styles page

--- thepinkpanther/src/components/Footer/index.js
+++ thepinkpanther/src/components/Footer/index.js
@@ -9,9 +9,9 @@ function Footer(props) {
             <div className="container">
                         <ul>
                         <label>Contributors:</label>
-                            <li><a href="https://github.com/Asix94">Carlos Ramos</a></li>
-                            <li><a href="https://github.com/AlexGonRod">Alex Gonzalez</a></li>
-                            <li><a href="https://github.com/JaimeRC">Jaime Rubio</a></li>
+                            <li><a className="colaborators" href="https://github.com/Asix94">Carlos Ramos</a></li>
+                            <li><a className="colaborators" href="https://github.com/AlexGonRod">Alex Gonzalez</a></li>
+                            <li><a className="colaborators" href="https://github.com/JaimeRC">Jaime Rubio</a></li>
                         </ul>
             </div>
         </footer>
